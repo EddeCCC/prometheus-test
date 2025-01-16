a483073f0de38b82262a26d4db239827f8cbe8c1
EddeCCC <eduard.schander@gmx.de>
2025-01-16 16:20:36 +0100

    start windows exporter as .exe

--- windows/install.bat
+++ windows/install.bat
@@ -1,10 +1,6 @@
 @echo off
-:: Check if IIS is already installed
-echo Checking for IIS installation...
 
-:: Enable IIS features via DISM
-dism /online /enable-feature /featurename:IIS-WebServerRole /all /norestart
-dism /online /enable-feature /featurename:IIS-WebServerManagementTools /all /norestart
+echo Checking for IIS installation...
 
 :: Install IIS if it's not installed already
 for /f "tokens=2 delims=:" %%I in ('dism /online /get-features ^| findstr /i "IIS-WebServerRole"') do set IISStatus=%%I
@@ -13,30 +9,23 @@ if /i "%IISStatus%" NEQ "Enabled" (
     dism /online /enable-feature /featurename:IIS-WebServerRole /all
     dism /online /enable-feature /featurename:IIS-WebServerManagementTools /all
 ) else (
-    echo IIS is already installed.
+    echo IIS is already installed
 )
 
 :: Start IIS if not already started
 echo Starting IIS...
 iisreset /start
 
-:: Check IIS status and notify user
-echo IIS should now be enabled and started.
+echo IIS should be enabled and started
 
-:: Starting windows_exporter service
-echo Starting windows_exporter service...
+:: Check if windows_exporter.exe exists
+if exist "windows_exporter-0.30.0-amd64.exe" (
+    echo Starting windows_exporter...
 
-:: Check if windows_exporter.exe exists in the same folder as this batch script
-if exist "%~dp0windows_exporter-0.30.0-amd64.exe" (
-    echo Found windows_exporter.exe, creating the service...
-
-    :: Create a new service for windows_exporter with IIS-collector
-    sc create windows_exporter binPath= "%~dp0windows_exporter-0.30.0-amd64.exe -log.file event --collectors.enabled iis,[defaults]" start= auto
-    sc start windows_exporter
-    echo windows_exporter service started.
+    :: Start the .exe and print output to terminal
+    start cmd /k windows_exporter-0.30.0-amd64.exe --collectors.enabled "iis,[defaults]"
 ) else (
-    echo windows_exporter.exe not found in the current folder. Make sure the file exists.
+    echo windows_exporter.exe not found
 )
 
-:: End of script
 pause
