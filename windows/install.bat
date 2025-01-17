2c31b44c7a39534c8433c5234e85c3c5899d2ac9
EddeCCC <eduard.schander@gmx.de>
2025-01-17 12:54:30 +0100

    add firewall rule

--- windows/install.bat
+++ windows/install.bat
@@ -20,6 +20,12 @@ echo IIS should be enabled and started
 
 :: Check if windows_exporter.exe exists
 if exist "%~dp0windows_exporter.exe" (
+
+    echo Adding firewall rule for windows_exporter...
+
+    :: Prometheus is not running on this windows machine, but needs to access the exposed metrics
+    netsh advfirewall firewall add rule name="windows_exporter inbound traffic" dir=in action=allow program="%~dp0windows_exporter.exe" enable=yes
+
     echo Starting windows_exporter...
 
     :: Start the exporter
@@ -33,4 +39,5 @@ if exist "%~dp0windows_exporter.exe" (
     echo windows_exporter.exe not found
 )
 
+:: Keep terminal open
 pause
