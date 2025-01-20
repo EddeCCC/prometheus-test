2a8062bec0c0c2bf1f5076c06e972a0ccc442d80
EddeCCC <eduard.schander@gmx.de>
2025-01-20 11:23:05 +0100

    add logs

--- node_exporter_setup.sh
+++ node_exporter_setup.sh
@@ -3,10 +3,13 @@
 mkdir -p /workspaces/node-exporter
 
 # Download the node-exporter
+echo Downloading node-exporter...
 wget -O /workspaces/node-exporter/node-exporter.tar.gz https://github.com/prometheus/node_exporter/releases/download/v1.8.2/node_exporter-1.8.2.linux-amd64.tar.gz
 
 # Extract the data
+echo Extracting node-exporter...
 tar xvfz /workspaces/node-exporter/node-exporter.tar.gz -C /workspaces/node-exporter
 
 # Start the exporter
+echo Starting node-exporter...
 /workspaces/node-exporter/node_exporter-1.8.2.linux-amd64/node_exporter
